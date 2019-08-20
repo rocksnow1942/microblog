ff3be0749b9debdef859461d2fea7c64709b6249
Hui Kang <rocksnow1942@gmail.com>
2019-08-19 21:02:47 -0700

    delete migration

--- app/models.py
+++ app/models.py
@@ -27,6 +27,13 @@ class User(UserMixin,db.Model):
         primaryjoin=(followers.c.follower_id==id),
         secondaryjoin=(followers.c.followed_id == id),
         backref=db.backref('followers',lazy='dynamic'),lazy='dynamic')
+    messages_sent = db.relationship('Message',foreign_keys='Message.sender_id',
+                    backref='author', lazy='dynamic')
+    messages_received = db.relationship('Message',foreign_keys='Message.recipient_id',
+                    backref='recipient', lazy='dynamic')
+    last_message_read_time = db.Column(db.DateTime)
+
+
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -71,6 +78,10 @@ class User(UserMixin,db.Model):
             return
         return User.query.get(id)
 
+    def new_messages(self):
+        last_read_time = self.last_message_read_time or datetime(1900,1,1)
+        return Message.query.filter_by(recipient=self).filter(Message.timestamp > last_read_time).count()
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -81,6 +92,17 @@ class Post(db.Model):
 
     def __repr__(self):
         return "<Post {}>".format(self.body)
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Message {}>'.format(self.body)
 
 
 @login.user_loader
